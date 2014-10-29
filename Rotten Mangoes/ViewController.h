6181491f71da5fab9e348ce1b8eb30268026229d
taylor.ledingham <taylor.ledingham@gmail.com>
2014-10-29 08:24:13 -0700

    Initial Commit

---
+++ Rotten Mangoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Mangoes
+//
+//  Created by Taylor Ledingham on 2014-10-29.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rotten MangoesTests/Rotten_MangoesTests.m
@@ -0,0 +1,40 @@
+//
+//  Rotten_MangoesTests.m
+//  Rotten MangoesTests
+//
+//  Created by Taylor Ledingham on 2014-10-29.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Rotten_MangoesTests : XCTestCase
+
+@end
+
+@implementation Rotten_MangoesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
